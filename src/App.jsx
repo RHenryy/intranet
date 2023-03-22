8e15f47411bd9432e391201abfdb9effed2ca86c
ElysiumTM <jacques.ham@outlook.fr>
2023-03-22 14:10:48 +0100

    created nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Users from './users'
+import { useEffect } from "react";
+import "./App.css";
+import Users from "./users";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "./js/userSlice";
+import Menu from "./component/Menu";
 
 function App() {
-const { data } = useSelector((state) => state.allUsers);
-const dispatch = useDispatch();
-useEffect(() => {
-  dispatch(fetchUsers())}, []);
+  const { data } = useSelector((state) => state.allUsers);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
   return (
     <div className="App">
-      {
-        data.length>0 && data.map((item, index) => {
+      <Menu />
+      {data.length > 0 &&
+        data.map((item, index) => {
           return (
-              <Users key={item.index} id={item.id} lastname={item.lastname} firstname={item.firstname} photo={item.photo} birthdate={item.age} email={item.email} city={item.city} country={item.country} phone={item.phone} category={item.category} birthday={item.birthday}/>
-          )
-        
-      })}
-      <button style={{ margin: "1rem" }} onClick={() => dispatch(fetchUsers())}>Fetch Users</button>
+            <Users
+              key={item.index}
+              id={item.id}
+              lastname={item.lastname}
+              firstname={item.firstname}
+              photo={item.photo}
+              birthdate={item.age}
+              email={item.email}
+              city={item.city}
+              country={item.country}
+              phone={item.phone}
+              category={item.category}
+              birthday={item.birthday}
+            />
+          );
+        })}
+      <button style={{ margin: "1rem" }} onClick={() => dispatch(fetchUsers())}>
+        Fetch Users
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
